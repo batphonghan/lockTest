ba0a4d0b4c2cf3ec8a035eee2f1d860166f8fefa
hung nguyen <hung.nguyen@eastagile.com>
2018-07-30 16:35:49 +0700

    Initial Commit

---
+++ testContact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testContact
+//
+//  Created by East Agile on 7/30/18.
+//  Copyright © 2018 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testContact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testContact
+//
+//  Created by East Agile on 7/30/18.
+//  Copyright © 2018 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
